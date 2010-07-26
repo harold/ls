2b4afe724f72f70e59aaef4e094f36ee326ce65c
Harold <hhausman@gmail.com>
2010-07-26 00:08:54 -0600

    DotIntrinsic is more powerful now. Some cute temporary hacks w/ winforms.

--- ls/Evaluator.cs
+++ ls/Evaluator.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace ls
 {
@@ -11,37 +12,122 @@ namespace ls
     {
         static Hashtable GlobalEnvironment = new Hashtable();
 
+        static Type SeriouslyGetType(Symbol inTypeName)
+        {
+            Type theType = Type.GetType(inTypeName.Name);
+            string[] theSuffixes = {
+                                       ", System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                                       ", System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
+                                   };
+
+            int theCursor = 0;
+            while (theType == null && theCursor < theSuffixes.Length)
+            {
+                theType = Type.GetType(string.Format("{0}{1}", inTypeName.Name, theSuffixes[theCursor]));
+                theCursor++;
+            }
+
+            return theType;
+        }
+
         static object NewIntrisic(ArrayList inList)
         {
-            string theClass = ((Symbol)inList[0]).Name;
-            return Activator.CreateInstance(Type.GetType(theClass));
+            ArrayList theArgs = new ArrayList();
+            for (int i = 1; i < inList.Count; i++)
+            {
+                theArgs.Add(inList[i]);
+            }
+
+            Type theType = SeriouslyGetType((Symbol)inList[0]);
+            ConstructorInfo constructor = theType.GetConstructor(Type.GetTypeArray(theArgs.ToArray()));
+
+            return constructor.Invoke(theArgs.ToArray());
         }
 
         static object DotIntrinsic(ArrayList inList)
         {
-            string theClass = ((Symbol)inList[0]).Name;
-            string theMethod = ((Symbol)inList[1]).Name;
+            object theTarget = null;
+            Type theType = null;
+
+            if (inList[0] is Symbol)
+            {
+                theType = SeriouslyGetType((Symbol)inList[0]);
+            }
+            else
+            {
+                theType = inList[0].GetType();
+                theTarget = inList[0];
+            }
+
+            string theMember = ((Symbol)inList[1]).Name;
 
             ArrayList theArgs = new ArrayList();
-
             for (int i = 2; i < inList.Count; i++)
             {
                 theArgs.Add(inList[i]);
             }
 
-            List<Type> theTypes = new List<Type>();
+            MethodInfo theMethodInfo = theType.GetMethod(theMember, Type.GetTypeArray(theArgs.ToArray()));
+            if (theMethodInfo != null)
+                return theMethodInfo.Invoke(theTarget, theArgs.ToArray());
 
-            foreach (object arg in theArgs)
+            if (theType != null)
             {
-                theTypes.Add(arg.GetType());
+                PropertyInfo property = theType.GetProperty(theMember);
+                if (property != null)
+                { // Didn't find a method, maybe it's a property.
+                    if (theArgs.Count > 0)
+                    {
+                        property.SetValue(theTarget, theArgs[0], null);
+                        return theArgs[0];
+                    }
+                    else
+                    {
+                        return property.GetValue(theTarget, null);
+                    }
+                }
+
+                FieldInfo field = theType.GetField(theMember);
+                if (field != null)
+                { // Not a property either, perhaps it's a field.
+                    if (theArgs.Count > 0)
+                    {
+                        field.SetValue(theTarget, theArgs[0]);
+                        return theArgs[0];
+                    }
+                    else
+                    {
+                        return field.GetValue(theTarget);
+                    }
+                }
+
+                return Enum.Parse(theType, ((Symbol)inList[1]).Name, true); // Final chance, are you an enum?
             }
 
-            object[] args = theArgs.ToArray();
-            Type[] argTypes = theTypes.ToArray();
+            return "DotIntrinsic: null type?";
+        }
 
-            Type type = Type.GetType(theClass);
-            MethodInfo method = type.GetMethod(theMethod, argTypes);
-            return method.Invoke(null, args);
+        static object WindowIntrinsic(ArrayList inList)
+        {
+            Form theForm = new Form();
+
+            if (inList.Count > 0 && inList[0] != null)
+                theForm.Text = (string)inList[0];
+
+            if (inList.Count > 1)
+                theForm.Width = (int)inList[1];
+
+            if (inList.Count > 2)
+                theForm.Height = (int)inList[2];
+
+            (new Thread(new ParameterizedThreadStart(LaunchWindow))).Start(theForm);
+            return theForm;
+        }
+
+        static void LaunchWindow(object inForm)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(inForm as Form);
         }
 
         static object PlusIntrinsic(ArrayList inList)
@@ -54,12 +140,20 @@ namespace ls
             return (int)Eval(inList[0]) * (int)Eval(inList[1]);
         }
 
+        static object EqualsIntrinsic(ArrayList inList)
+        {
+            return inList[0].Equals(inList[1]);
+        }
+
         static Evaluator()
         {
             GlobalEnvironment.Add(new Symbol("new").Name, new MetaFunction(NewIntrisic));
             GlobalEnvironment.Add(new Symbol(".").Name, new MetaFunction(DotIntrinsic));
+            GlobalEnvironment.Add(new Symbol("window").Name, new MetaFunction(WindowIntrinsic));
+
             GlobalEnvironment.Add(new Symbol("+").Name, new MetaFunction(PlusIntrinsic));
             GlobalEnvironment.Add(new Symbol("*").Name, new MetaFunction(StarIntrinsic));
+            GlobalEnvironment.Add(new Symbol("=").Name, new MetaFunction(EqualsIntrinsic));
 
             GlobalEnvironment.Add(new Symbol("true").Name, true);
             GlobalEnvironment.Add(new Symbol("false").Name, false);
--- ls/Program.cs
+++ ls/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace ls
 {
@@ -6,9 +7,18 @@ namespace ls
     {
         static void Main(string[] args)
         {
+            Evaluator.Eval(Reader.Read("(define box (fn (contents title) (. 'System.Windows.Forms.MessageBox 'Show contents title (. 'System.Windows.Forms.MessageBoxButtons 'OKCancel) (. 'System.Windows.Forms.MessageBoxIcon 'Error)))))"));
             Evaluator.Eval(Reader.Read("(define file-text (fn (path) (. 'System.IO.File 'ReadAllText path)))"));
             Evaluator.Eval(Reader.Read("(define read (fn (read-str) (. 'ls.Reader 'Read read-str))"));
             Evaluator.Eval(Reader.Read("(define eval (fn (eval-str) (. 'ls.Evaluator 'Eval eval-str))"));
+
+            if (System.IO.File.Exists("main.ls"))
+            {
+                StringReader sr = new StringReader(System.IO.File.ReadAllText("main.ls"));
+                while (sr.Peek() != -1)
+                    Evaluator.Eval(Reader.Read(sr));
+            }
+
             while (true)
             {
                 Console.Write("> ");
